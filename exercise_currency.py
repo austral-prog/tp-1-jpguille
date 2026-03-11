73ee354acd684a0b63b38c6f41e2a78f1f9854d2
jpguille <jpguillen2008@gmail.com>
2026-03-11 00:59:35 -0300

    Update exercise_currency.py

--- exercise_currency.py
+++ exercise_currency.py
@@ -11,3 +11,11 @@ def currency():
     tasa_dolar = 1500  # 1 dólar = 1500 pesos
     tasa_euro = 1600   # 1 euro = 1600 pesos
     tasa_real = 250    # 1 real = 250 pesos
+    
+    monto_dolares = pesos / tasa_dolar
+    monto_euro = pesos / tasa_euro
+    monto_real = pesos / tasa_real
+
+    print(monto_dolares)
+    print(monto_euro)
+    print(monto_real)
